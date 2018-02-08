f15cdf096c1aaefa5ad4449d97a3c39a7de2026e
Dmitry Levanov <mitrskoggson@yandex.ru>
2018-02-08 13:36:56 +0300

    Created base project structure.

--- app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
